a7ee1f27a93ce4990cda2b5455ce16cce480c32f
Sonal <sonal.2015347@iit.ac.lk>
2017-02-15 11:54:44 +0530

    Register

--- templatepage.php
+++ templatepage.php
@@ -74,32 +74,6 @@ include ("headfile.html");
 //inut
 //include head layout
 include("footfile.html");
-//include head layout 
-include ("headfile.html");
-
-//include head layout
-
-
-//inut
-//include head layout
-include("footfile.html");
-//include head layout 
-include ("headfile.html");
-
-//include head layout
-
-
-//inut
-//include head layout
-include("footfile.html");
-//include head layout 
-include ("headfile.html");
-
-//include head layout
-
-
-//inut
-//include head layout
-include("footfile.html");
+//include head layou
 
 ?>
