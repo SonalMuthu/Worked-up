36acb624551716426ad50cd8c6f019af03684e78
Sonal <sonal.2015347@iit.ac.lk>
2017-02-20 11:58:07 +0530

    Register fix

--- templatepage.php
+++ templatepage.php
@@ -71,26 +71,15 @@ include ("headfile.html");
 //include head layout
 
 
-//inut
+//include head layout
+
+echo "<p></p>";
+//display name of the page and some random text
+echo "<h2>".$pagename."</h2>";
+echo "<p> Text Here";
+
+//include head layout
 //include head layout
 include("footfile.html");
-//include head layout 
-include ("headfile.html");
-
-//include head layout
-
-
-//inut
-//include head layout
-include("footfile.html");//include head layout 
-include ("headfile.html");
-
-//include head layout
-
-
-//inut
-//include head layout
-include("footfile.html");
-
 
 ?>
