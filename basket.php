1de405d40709f5abec4a1052e7107e84fb2972d1
Sonal <sonal.2015347@iit.ac.lk>
2017-02-23 12:05:00 +0530

    Basket

--- basket.php
+++ basket.php
@@ -1,15 +1,4 @@
 <?php
-session_start();
-include("db.php");
-//create a variable called $pagename which contains the actual name of the page
-$pagename="Ordering Basket";
-@$newprodid=$_POST['h_prodid'];
-@$reququantity= $_POST['stock'];
-//call in the style sheet called ystylesheet.css to format the page as defined in the style sheet
-echo "<link rel=stylesheet type=text/css href=mystylesheet.css>";
-
-//display window title
-echo "<title>".$pagename."</title>";
 //include head layout 
 include ("headfile.html");
 include ("detectlogin.php");
